6a90ed56595d6b8a9375eddaf4a4e3cb0d5dcea7
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2025-03-15 12:44:11 +0200

    add auth service

--- go.mod
+++ go.mod
@@ -9,6 +9,12 @@ require (
 )
 
 require (
+	cloud.google.com/go/compute v1.20.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
@@ -20,7 +26,9 @@ require (
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/oauth2 v0.17.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 )
 
 require (
@@ -34,9 +42,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/gorilla/sessions v1.4.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/markbates/goth v1.80.0
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
---
+++ internal/auth/auth_service.go
@@ -0,0 +1,179 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+	"github.com/vladovsiychuk/auth-service-go/pkg/helper"
+)
+
+type AuthService struct {
+	keyRepository          KeyRepositoryI
+	sessionTokenRepository SessionTokenRepositoryI
+}
+
+type AuthServiceI interface {
+	GenerateJwtAndSessionTokens(email string) (string, SessionTokenI, error)
+	RefreshJwtAndSessionTokens(sessionTokenId uuid.UUID) (string, SessionTokenI, error)
+	TokenIsValid(tokenStr string) bool
+}
+
+func NewService(keyRepository KeyRepositoryI, sessionTokenRepository SessionTokenRepositoryI) *AuthService {
+	return &AuthService{
+		keyRepository,
+		sessionTokenRepository,
+	}
+}
+
+func (s *AuthService) Init() {
+	initOauthProviders()
+
+	keys := CreateKeys()
+
+	if err := s.keyRepository.Update(keys); err != nil {
+		panic("Can't save keys in the repository.")
+	}
+
+	s.startKeyRotation()
+}
+
+func (s *AuthService) startKeyRotation() {
+	go func() {
+		ticker := time.NewTicker(JWT_KEYS_DURATION)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			keys, _ := s.keyRepository.GetKeys()
+			keys.Rotate()
+			s.keyRepository.Update(keys)
+		}
+	}()
+}
+
+func initOauthProviders() {
+	sessionSecret := helper.GetEnv("SESSION_SECRET", "")
+	googleClientKey := helper.GetEnv("GOOGLE_OAUTH_CLIENT_KEY", "")
+	googleSecret := helper.GetEnv("GOOGLE_OAUTH_SECRET", "")
+
+	gothic.Store = sessions.NewCookieStore([]byte(sessionSecret))
+
+	goth.UseProviders(
+		google.New(
+			googleClientKey,
+			googleSecret,
+			"http://localhost:8080/auth/callback",
+			"email", "profile",
+		),
+	)
+}
+
+func (s *AuthService) GenerateJwtAndSessionTokens(email string) (string, SessionTokenI, error) {
+	jwtTokenStr, err := s.generateJwtTokenStr(email)
+	if err != nil {
+		return "", nil, err
+	}
+
+	sessionToken := CreateSessionToken(email)
+	if err := s.sessionTokenRepository.Create(sessionToken); err != nil {
+		return "", nil, err
+	}
+
+	return jwtTokenStr, sessionToken, err
+}
+
+func (s *AuthService) RefreshJwtAndSessionTokens(sessionTokenId uuid.UUID) (string, SessionTokenI, error) {
+	currentSessionTokenI, err := s.sessionTokenRepository.FindById(sessionTokenId)
+	currentSessionToken := currentSessionTokenI.(*SessionToken)
+	if err != nil {
+		// Something went wrong, the session token was most likely stolen.
+		// Because it's quite strange that frontend has sent a token that was deleted
+		// if it was deleted means that it was used.
+		// The best thing to do here is to delete all current user sessions (all session tokens by user id/email)
+		// from the repository.
+		// And redirect the user to the login page (return error)
+		return "", nil, err
+	}
+
+	newSessionToken := CreateSessionToken(currentSessionToken.Email)
+	newJwtTokenStr, err := s.generateJwtTokenStr(currentSessionToken.Email)
+	if err != nil {
+		return "", nil, err
+	}
+
+	if err := s.sessionTokenRepository.Delete(currentSessionTokenI); err != nil {
+		return "", nil, err
+	}
+
+	if err := s.sessionTokenRepository.Create(newSessionToken); err != nil {
+		return "", nil, err
+	}
+
+	return newJwtTokenStr, newSessionToken, nil
+}
+
+func (s *AuthService) TokenIsValid(tokenStr string) bool {
+	keys, err := s.keyRepository.GetKeys()
+	if err != nil {
+		fmt.Printf("Failed to load keys from repository.")
+		return false
+	}
+
+	publicKey, err := keys.GetPublicKey()
+	if err != nil {
+		fmt.Printf("Failed to decode public key.")
+		return false
+	}
+
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return publicKey, nil
+	})
+
+	if err != nil || !token.Valid {
+		secondayPublicKey, err := keys.GetSecondaryPulicKey()
+		if err != nil {
+			fmt.Printf("Failed to decode secondary public key.")
+			return false
+		}
+
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return secondayPublicKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			fmt.Printf("Token is not valid.")
+			return false
+		}
+	}
+
+	return true
+}
+
+func (s *AuthService) generateJwtTokenStr(email string) (string, error) {
+	keys, err := s.keyRepository.GetKeys()
+	if err != nil {
+		return "", err
+	}
+
+	privateKey, err := keys.GetPrivateKey()
+	if err != nil {
+		return "", err
+	}
+
+	claims := jwt.MapClaims{
+		"email": email,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	return token.SignedString(privateKey)
+}
